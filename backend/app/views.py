2982a0c8dcb0b5b1354262563cc1c7b3aae28d56
Gustavo Sato <gustavosato79@gmail.com>
2022-10-10 18:32:50 -0300

    projeto de cadastro de aulas e professores

---
+++ backend/app/views.py
@@ -0,0 +1,6 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HomeApiView(APIView):
+    def get(self, request, format=None):
+        return Response({"nome": "teste", "idade": 26}, status=200)
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,47 @@
+import { Box } from '@mui/material'
+import type { NextPage } from 'next'
+import { Professor } from '../src/@types/professor'
+import Lista from '../src/components/Lista/Lista'
+
+const Home: NextPage = () => {
+  const professores: Professor[] = [
+    {
+      id: 1,
+      nome: "Professor 1",
+      foto: "https://www.imagensempng.com.br/wp-content/uploads/2021/08/Empresario-Png-1024x1024.png",
+      descricao: "Descrição do professor 1",
+      valor_hora: 100
+    },
+    {
+      id: 2,
+      nome: "Professor 2",
+      foto: "https://www.imagensempng.com.br/wp-content/uploads/2021/08/Empresario-Png-1024x1024.png",
+      descricao: "Descrição do professor 2",
+      valor_hora: 200
+    },
+    {
+      id: 3,
+      nome: "Professor 3",
+      foto: "https://www.imagensempng.com.br/wp-content/uploads/2021/08/Empresario-Png-1024x1024.png",
+      descricao: "Descrição do professor 3",
+      valor_hora: 300
+    },
+    {
+      id: 4,
+      nome: "Professor 4",
+      foto: "https://www.imagensempng.com.br/wp-content/uploads/2021/08/Empresario-Png-1024x1024.png",
+      descricao: "Descrição do professor 4",
+      valor_hora: 400
+    },
+
+  ]
+
+
+  return (
+    <Box sx={{backgroundColor: 'secondary.main'}}>
+      <Lista professores={professores}></Lista>
+    </Box>
+  )
+}
+
+export default Home
